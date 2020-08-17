513e9242aaa3aecec07640b7e9e20e7feec634e0
mandy <andreahk@163.com>
2020-08-17 16:23:28 +0800

    other files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,79 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-nodejs-app';
+  theme = false;
+  mode = false;
+  dark = false;
+  menus = [
+    {
+      level: 1,
+      title: 'Mail Group',
+      icon: 'mail',
+      open: true,
+      selected: false,
+      disabled: false,
+      children: [
+        {
+          level: 2,
+          title: 'Group 1',
+          icon: 'bars',
+          open: false,
+          selected: false,
+          disabled: false,
+          children: [
+            {
+              level: 3,
+              title: 'Option 1',
+              selected: false,
+              disabled: false
+            },
+            {
+              level: 3,
+              title: 'Option 2',
+              selected: false,
+              disabled: true
+            }
+          ]
+        },
+        {
+          level: 2,
+          title: 'Group 2',
+          icon: 'bars',
+          selected: true,
+          disabled: false
+        },
+        {
+          level: 2,
+          title: 'Group 3',
+          icon: 'bars',
+          selected: false,
+          disabled: false
+        }
+      ]
+    },
+    {
+      level: 1,
+      title: 'Team Group',
+      icon: 'team',
+      open: false,
+      selected: false,
+      disabled: false,
+      children: [
+        {
+          level: 2,
+          title: 'User 1',
+          icon: 'user',
+          selected: false,
+          disabled: false
+        },
+        {
+          level: 2,
+          title: 'User 2',
+          icon: 'user',
+          selected: false,
+          disabled: false
+        }
+      ]
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {EmployeeCreateComponent} from './components/employee-create/employee-create.component';
+import {EmployeeEditComponent} from './components/employee-edit/employee-edit.component';
+import {EmployeeListComponent} from './components/employee-list/employee-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ApiService} from "./service/api.service";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {en_US, NgZorroAntdModule, NZ_I18N} from "ng-zorro-antd";
+
+import {registerLocaleData} from '@angular/common';
+import en from '@angular/common/locales/en';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeCreateComponent,
+    EmployeeEditComponent,
+    EmployeeListComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NgZorroAntdModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    ApiService,
+    {provide: NZ_I18N, useValue: en_US}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
